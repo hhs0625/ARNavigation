c2c651e1712ba6661d3559a1cb79bd86af7fd459
hhs0625 <hhs0625@gmail.com>
2019-09-30 19:13:15 +0900

    init touch
    - GPS 좌표 받기
    - 목표 좌표 간 거리 계산
    - 북쪽 가르키는 나침판

---
+++ Assets/Script/compass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class compass : MonoBehaviour
+{
+    Quaternion currentHeading;
+
+    void Start()
+    {
+        Input.location.Start();
+        Input.compass.enabled = true;
+    }
+
+    void Update()
+    {
+        currentHeading = Quaternion.Euler(0, 0, Input.compass.trueHeading);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation,
+            currentHeading, Time.deltaTime * 2f);
+    }
+}
